7c3dcb1fff34255e97d9952c64dd08f69202ea30
realex3355 <realex3355@gmail.com>
2023-05-18 14:47:51 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+const colorButton = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+colorButton.addEventListener("click", () => {
+  let currentColor = getRandomHexColor();
+  body.style.backgroundColor = currentColor;
+  spanColor.textContent = currentColor;
+});

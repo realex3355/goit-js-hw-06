23f71280bfd78d532413d4dc6d82f7b3f6d5ad4d
realex3355 <realex3355@gmail.com>
2023-05-18 14:44:37 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+  if (email !== "" && password !== "") {
+    const enteredData = {
+      email,
+      password,
+    };
+    console.log(enteredData);
+    loginForm.reset();
+  } else {
+    alert("All fields must be filled!");
+  }
+});
